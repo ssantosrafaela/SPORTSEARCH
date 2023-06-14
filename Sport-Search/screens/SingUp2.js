a609f1ad6dc1e5819997919cffb12b8801e43510
Rafaela dos Santos <rafaeladasilvadossantos9@gmail.com>
2023-06-15 00:11:47 +0530

    first

---
+++ Sport-Search/screens/SingUp2.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity as TO } from "react-native";
+import Entrada from "../components/Entrada";
+import Checkbox from 'expo-checkbox';
+
+export default function SingUp2() {
+  const [textEmail, setEmail] = useState(" ");
+  const [textConfirmEmail, setConfirmEmail] = useState(" ");
+  const [textUser, setUser] = useState(" ");
+  const [textPassword, setPassword] = useState(" ");
+  const [textConfirmPassword, setConfirmPassword] = useState(" ");
+
+  const [isSelected, setSelection] = useState(false);
+
+  const register = "Cadastrar";
+  return (
+    <>
+      <View style={styles.container}>
+        <Entrada
+          label="Digite seu e-mail"
+          setValue={setEmail}
+          value={textEmail}
+        />
+        <Entrada
+          label="Digite sua senha"
+          setValue={setPassword}
+          value={textPassword}
+        />
+
+        <Entrada label="Usuário" setValue={setUser} value={textUser} />
+
+        <Entrada
+          label="Confirme seu e-mail"
+          setValue={setConfirmEmail}
+          value={textConfirmEmail}
+        />
+        <Entrada
+          label=" "
+          setValue={setConfirmPassword}
+          value={textConfirmPassword}
+        />
+
+        <View>
+          <TO>
+            <Text>{register}</Text>
+          </TO>
+
+          <View style={styles.checkboxContainer}>
+            <Checkbox
+              value={isSelected}
+              onValueChange={setSelection}
+              style={styles.checkbox}
+            />
+            <Text>Li e aceito os termos de condições</Text>
+          </View>
+
+          <View style = {styles.text}>
+            <Text>Já se cadastrou? Entre</Text>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#83A1EF",
+    justifycontent: "center",
+    alignItems: "center",
+  },
+  to: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 20,
+    width: 60,
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+  },
+  checkbox: {
+    alignSelf: "center",
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+});
